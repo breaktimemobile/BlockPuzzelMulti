27004152e941b10385f561ad1d4d9d4f5e56f942
MinSeok <hsjms1201@gmail.com>
2020-06-22 15:59:34 +0900

    ADS

--- Assets/03.Scripts/Manager/AdsManager.cs
+++ Assets/03.Scripts/Manager/AdsManager.cs
@@ -71,7 +71,7 @@ public class AdsManager : MonoBehaviour
 #elif UNITY_ANDROID
         string adUnitId = "ca-app-pub-4682698622407711/4585067879";
 #elif UNITY_IPHONE
-        string adUnitId = "ca-app-pub-3940256099942544/2934735716";
+        string adUnitId = "ca-app-pub-3940256099942544~3347511713";
 #else
         string adUnitId = "unexpected_platform";
 #endif
