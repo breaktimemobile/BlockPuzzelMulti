7c39ea4e833f60b5a7f07cf3aa2815468f706f83
MinSeok <hsjms1201@gmail.com>
2020-06-23 10:56:22 +0900

    firebase add

--- Assets/03.Scripts/Manager/FireBaseManager.cs
+++ Assets/03.Scripts/Manager/FireBaseManager.cs
@@ -21,7 +21,7 @@ public class FireBaseManager : MonoBehaviour
     {
 
         // Log an event with no parameters.
-        //FirebaseAnalytics.LogEvent(MainTitle);
+        FirebaseAnalytics.LogEvent(MainTitle);
 
 
     }
@@ -30,7 +30,7 @@ public class FireBaseManager : MonoBehaviour
     {
 
         // Log an event with no parameters.
-        //FirebaseAnalytics.LogEvent(MainTitle, SubTitle, val);
+        FirebaseAnalytics.LogEvent(MainTitle, SubTitle, val);
 
 
     }
